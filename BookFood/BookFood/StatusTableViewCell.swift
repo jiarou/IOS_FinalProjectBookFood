38bb49482d93d6e894b6e174a4de3aa7fa97e21b
jiarou <102306007@nccu.edu.tw>
2017-06-21 03:54:47 +0800

    Data connection between firebase and moltin

---
+++ BookFood/BookFood/StatusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatusTableViewCell.swift
+//  BookFood
+//
+//  Created by jiarou on 2017/6/21.
+//  Copyright © 2017年 teamFour. All rights reserved.
+//
+
+import UIKit
+
+class StatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
